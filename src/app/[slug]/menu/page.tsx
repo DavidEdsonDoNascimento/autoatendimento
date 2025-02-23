533462be55152dd35dea74ae128b14729296b3a4
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2025-02-23 13:32:04 -0300

    feat: criado scrool bar com as categorias do restaurante

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -3,9 +3,10 @@ import { notFound } from 'next/navigation';
 
 import { getRestaurantBySlug } from '@/data/get-restaurant-by-slug';
 
+import RestaurantCategories from './components/categories';
 import RestaurantHeader from './components/header';
 /**
- * Para pegar os paramemtros que vem da URL(slug e o consumptionMethod)
+ * Para pegar os parâmetros que vem da URL(slug e o consumptionMethod)
  * É obrigatório o nome params e searchParams para que funcione
  */
 interface MenuPageProps {
@@ -30,13 +31,10 @@ const MenuPage = async ({ params, searchParams }: MenuPageProps) => {
 	if (!restaurant) {
 		return notFound();
 	}
-
 	return (
 		<div>
 			<RestaurantHeader restaurant={restaurant} />
-			<div>
-				<h1>{slug}</h1>
-			</div>
+			<RestaurantCategories restaurant={restaurant} />
 		</div>
 	);
 };

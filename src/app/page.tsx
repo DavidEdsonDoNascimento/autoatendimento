1d81aa120eba3226fdd27b64b0975fd26b4f2a4a
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2025-02-28 17:50:19 -0300

    style: ajuste na pagina home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { db } from '@/lib/prisma';
@@ -9,7 +10,7 @@ const HomePage = async () => {
 		<div className='flex flex-col items-center'>
 			<div className='w-full text-center h-[120px] p-5'>
 				<h3 className='text-lg font-semibold'>
-					Selecione o restaurante cadastrado:
+					Selecione algum restaurante da lista:
 				</h3>
 			</div>
 			<div className='flex flex-col'>
@@ -18,9 +19,17 @@ const HomePage = async () => {
 						<Link
 							href={`/${r.slug}`}
 							key={r.id}
-							className='h-16 bg-red-400 p-5 border rounded-sm text-white'
+							className='w-full bg-orange-500 p-5 border rounded-sm text-white'
 						>
-							{r.name}
+							<div className='flex flex-col items-center gap-2'>
+								<Image
+									src={r.avatarImageUrl}
+									alt={r.name}
+									width={82}
+									height={82}
+								/>
+								<h2 className='font-semibold'>{r.name}</h2>
+							</div>
 						</Link>
 					);
 				})}
